379061c2d987417a8bba9e6479ab7f2f04983ce7
68254622 <juanpmesa99@gmail.com>
2020-07-27 15:19:06 -0500

    Updates

---
+++ precache-manifest.0dc51c1a4fd84c8d89d3e9bd5afa5fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec142d89a7d5fe873bbdf34c05246e9",
+    "url": "/goalslist-react/index.html"
+  },
+  {
+    "revision": "31972c0e72f8498f59b6",
+    "url": "/goalslist-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67f0a83d5ec90c0a78db",
+    "url": "/goalslist-react/static/js/2.ebb06584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goalslist-react/static/js/2.ebb06584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31972c0e72f8498f59b6",
+    "url": "/goalslist-react/static/js/main.cd0565b5.chunk.js"
+  },
+  {
+    "revision": "a71986de7950ef7c2478",
+    "url": "/goalslist-react/static/js/runtime-main.e93a0863.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goalslist-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
