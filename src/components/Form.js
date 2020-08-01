dbdf6d5e3a86fb6f4f90d741f0fefff8bed27c2e
68254622 <juanpmesa99@gmail.com>
2020-07-31 21:11:48 -0500

    Create HTML form

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,13 +32,36 @@ export default class Form extends Component {
 
     save = () => {
         this.props.addGoal(this.state);
-        $('#name, #cost, #saved').val('');
+        $('#info').find('input').val('');
     }
 
     render() {
         return (
-            <div>
-                
+            <div className="form-block col-sm-4">
+                <h2 className="title">Goal info</h2>
+                <form id="info">
+                    <div className="form-group">
+                        <div className="form-row">
+                            <div className="col">
+                                <label for="txtName">Name</label>
+                                <input type="text" id="txtName" className="form-control" placeholder="Name of the saving goal" onChange={this.nameHandler} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-row">
+                            <div className="col">
+                                <label for="txtCost">Cost</label>
+                                <input type="number" id="txtCost" className="form-control" placeholder="Cost to achieve the goal" onChange={this.costHandler} />
+                            </div>
+                            <div className="col">
+                                <label for="txtSavings">Savings</label>
+                                <input type="number" id="txtSavings" className="form-control" placeholder="Actual savings amount" onChange={this.savedHandler} />
+                            </div>
+                        </div>
+                    </div>
+                    <button type="button" id="add" className="btn btn-block btn-primary" onClick={this.save}>Add to list</button>
+                </form>
             </div>
         )
     }
