47ae8b72215c0e836cffe537996f81f2939f474d
68254622 <juanpmesa99@gmail.com>
2020-07-31 21:32:19 -0500

    Create goals list container

---
+++ src/components/Goals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+export default function Goals({goals}) {
+    return (
+        <div className="form-block col-sm-8">
+            <h2 className="title">Goals</h2>
+            <ul>
+                {goals.map((goal, index) => <Goal key={index} goal={goal} />)}
+            </ul>
+        </div>
+    )
+}
